3db7d5fe11ce8d9ea77abac87a58bd272fbd7ad6
VatsonP <58036174+VatsonP@users.noreply.github.com>
2021-01-14 23:20:51 +0200

    Add DataProviders to fill ValidCustomers

---
+++ RestSharpDemo/Model/DataProviders.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+
+namespace RestSharpDemo.Model
+{
+    internal class DataProviders
+    {
+        public static IEnumerable ValidCustomers
+        {
+            get
+            {   
+                /*
+                //1,Erda,Birkin,ebirkinb @google.com.hk,Aquamarine
+                yield return new DemoCust()
+                {
+                    id = "1",
+                    firstName = "Erda",
+                    lastName = "Birkin",
+                    email = "ebirkinb@google.com",
+                    favColor = "Aquamarine"
+                };
+                //2,Cherey,Endacott,cendacottc @freewebs.com,Fuscia
+                yield return new DemoCust()
+                {
+                    id = "2",
+                    firstName = "Cherey",
+                    lastName = "Endacott",
+                    email = "cendacottc@freewebs.com",
+                    favColor = "Fuscia"
+                };
+                */
+                //3,Shalom,Westoff,swestoffd @about.me,Red
+                yield return new DemoCust()
+                {
+                    id = "3",
+                    firstName = "Shalom",
+                    lastName = "Westoff",
+                    email = "swestoffd@about.me",
+                    favColor = "Red"
+                };
+                /*
+                //4,Jo,Goulborne,jgoulbornee @example.com,Red
+                yield return new DemoCust()
+                {
+                    id = "4",
+                    firstName = "Jo",
+                    lastName = "Goulborne",
+                    email = "jgoulbornee@example.com",
+                    favColor = "Red"
+                };
+                */
+                /*
+                 ... 
+                */
+            }
+        }
+    }
+}
